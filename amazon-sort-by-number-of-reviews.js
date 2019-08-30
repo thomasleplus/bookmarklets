2ee6b0d01c4ef08da6222846f6baafa6ef482255
Thomas Leplus <thomasleplus@users.noreply.github.com>
2019-08-30 10:59:38 -0700

    Create amazon-sort-by-number-of-reviews.js

---
+++ amazon-sort-by-number-of-reviews.js
@@ -0,0 +1,1 @@
+void((function(){ var url = new URL(location.href); var search_params = new URLSearchParams(url.search);  search_params.set('s', 'review-count-rank'); url.search = search_params.toString(); location.href = url.toString(); })());
