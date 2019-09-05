c9ab7c393b34554224d1f2402c4d11aa5dfbd70e
Thomas Leplus <thomasleplus@users.noreply.github.com>
2019-09-05 12:01:50 -0700

    Update sort-by-number-of-reviews.js

--- javascripts/amazon/sort-by-number-of-reviews.js
+++ javascripts/amazon/sort-by-number-of-reviews.js
@@ -1,10 +1,8 @@
-function () {
-  var url = new URL(location.href);
-  var search_params = new URLSearchParams(url.search);
-  if (!search_params.get('rh')) {
-    window.alert('Please narrow down your search to a specific department for the sort to take effect.');
-  }
-  search_params.set('s', 'review-count-rank');
-  url.search = search_params.toString();
-  location.href = url.toString();
+var url = new URL(location.href);
+var search_params = new URLSearchParams(url.search);
+if (!search_params.get('rh')) {
+  window.alert('Please narrow down your search to a specific department for the sort to take effect.');
 }
+search_params.set('s', 'review-count-rank');
+url.search = search_params.toString();
+location.href = url.toString();
