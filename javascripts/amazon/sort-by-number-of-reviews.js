36ecbe6b7dd9e4efe88c31d28305c6555d35f4b0
Thomas Leplus <thomasleplus@users.noreply.github.com>
2019-09-05 11:53:38 -0700

    Adding warning if current search does not have department criteria.

--- javascripts/amazon/sort-by-number-of-reviews.js
+++ javascripts/amazon/sort-by-number-of-reviews.js
@@ -1,6 +1,9 @@
 function () {
   var url = new URL(location.href);
   var search_params = new URLSearchParams(url.search);
+  if (!search_params.get('rh')) {
+    window.alert("You need to narrow down your search to a specific department for the sort to take effect.");
+  }
   search_params.set('s', 'review-count-rank');
   url.search = search_params.toString();
   location.href = url.toString();
