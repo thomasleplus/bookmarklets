a08643e208ea7e5539c9c5e0ff3d3cf59483c70b
Thomas Leplus <thomasleplus@users.noreply.github.com>
2019-09-05 12:12:47 -0700

    Update Sort by number of reviews.js

--- javascripts/Amazon/Sort by number of reviews.js
+++ javascripts/Amazon/Sort by number of reviews.js
@@ -1,6 +1,6 @@
 var url = new URL(location.href);
 var params = new URLSearchParams(url.search);
-if (!params.get('rh')) {
+if (!params.get('rh') && !params.get('i')) {
   window.alert('Please narrow down your search to a specific department for the sort to take effect.');
 }
 if (params.get('s') === 'review-count-rank') {
