3df062663b559498608e0907dc5846c43b41385a
Thomas Leplus <thomasleplus@users.noreply.github.com>
2019-09-05 19:44:02 -0700

    Update Whois.js

--- javascripts/Whois.js
+++ javascripts/Whois.js
@@ -1,1 +1,1 @@
-window.location.href = 'http://whois.domaintools.com/' + encodeURIComponent(window.location.hostname);
+location.href = 'http://whois.domaintools.com/' + encodeURIComponent(location.hostname);
