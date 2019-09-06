ce1cdd2703da8311992e8e69567663c0069b0b92
Thomas Leplus <thomasleplus@users.noreply.github.com>
2019-09-05 19:43:05 -0700

    Update Translate to English.js

--- javascripts/Translate to English.js
+++ javascripts/Translate to English.js
@@ -1,1 +1,1 @@
-window.location = 'http://translate.google.com/translate?langpair=auto%7Cen&u=' + encodeURIComponent(window.location);
+window.location.href = 'http://translate.google.com/translate?langpair=auto%7Cen&u=' + encodeURIComponent(window.location.href);
