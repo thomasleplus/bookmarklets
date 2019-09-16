254fe4dab3a1f063dd30104ade7be5823020aaf1
Thomas Leplus <thomasleplus@users.noreply.github.com>
2019-09-16 11:02:18 -0700

    Update Allow save password.js

--- javascripts/Allow save password.js
+++ javascripts/Allow save password.js
@@ -1,5 +1,7 @@
 if (document.hasFocus()) {
-  document.activeElement.autocomplete = 'on';
-} else {
-  window.alert('Move cursor in password field.');
+  if (document.activeElement.autocomplete) {
+    document.activeElement.autocomplete = 'on';
+    return;
+  }
 }
+window.alert('Move cursor to desired password field.');
