d7856c8fac4acb3942437c111ec7473eb51d8fc2
Thomas Leplus <thomasleplus@users.noreply.github.com>
2021-10-13 08:32:26 -0700

    Update Go to CVE.js

--- javascripts/Go to CVE.js
+++ javascripts/Go to CVE.js
@@ -2,21 +2,22 @@
 var selection;
 if (window.getSelection) {
     selection = window.getSelection().toString();
-} else if (document.selection && document.selection.type != "Control") {
+} else if (document.selection && document.selection.type !== "Control") {
     selection = document.selection.createRange().text;
 }
-if (typeof selection === 'undefined') {
+if (selection === undefined) {
     window.alert('Select the desired text.');
-    return;
-}
-selection = selection.trim();
-var url;
-if (selection.match(/^cve\-\d+\-\d+$/i)) {
-    url = 'https://nvd.nist.gov/vuln/detail/' + selection;
-} else if (selection.match(/^\d+\-\d+$/)) {
-    url = 'https://nvd.nist.gov/vuln/detail/CVE-' + selection;
 } else {
-    window.alert('Selection is not a valid CVE number.');
-    return;
+    selection = selection.trim();
+    var url;
+    if (selection.match(/^cve\-\d+\-\d+$/i)) {
+        url = 'https://nvd.nist.gov/vuln/detail/' + selection;
+    } else if (selection.match(/^\d+\-\d+$/)) {
+        url = 'https://nvd.nist.gov/vuln/detail/CVE-' + selection;
+    } else {
+        window.alert('Selection is not a valid CVE number.');
+    }
+    if (url === undefined) {
+        window.open(url, '_blank').focus();
+    }
 }
-window.open(url, '_blank').focus();
