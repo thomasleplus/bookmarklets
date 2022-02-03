f5b4b0d6dc37c04a3ff79001ebf9d366ebc8e3e6
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-02-03 09:41:40 -0800

    Update Allow save password.js

--- javascripts/Allow save password.js
+++ javascripts/Allow save password.js
@@ -3,7 +3,7 @@ var all = document.getElementsByTagName("*");
 var i;
 var max;
 for (i = 0, max = all.length; i < max; i++) {
-  if (all[i].autocomplete) {
-    all[i].autocomplete = 'on';
-  }
+    if (all[i].autocomplete) {
+        all[i].autocomplete = 'on';
+    }
 }
