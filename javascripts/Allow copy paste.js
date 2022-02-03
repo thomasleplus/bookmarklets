de0b5160193d0ebe1e63bbddce91a6f4617d7126
Thomas Leplus <thomasleplus@users.noreply.github.com>
2022-02-03 09:44:56 -0800

    Update Allow copy paste.js

--- javascripts/Allow copy paste.js
+++ javascripts/Allow copy paste.js
@@ -2,7 +2,7 @@
 var all = document.getElementsByTagName("*");
 var i;
 var max;
-for (i = 0, max = all.length; i < max; i++) {
+for (i = 0, max = all.length; i < max; i += 1) {
     if (all[i].onCopy) {
         all[i].onCopy = null;
     }
