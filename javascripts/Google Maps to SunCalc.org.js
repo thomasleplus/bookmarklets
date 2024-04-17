3e972f5d8f254741565f68b9c1dc76df79bd0183
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-04-17 08:21:01 -0300

    Add Google Maps to Suncalc.org

---
+++ javascripts/Google Maps to SunCalc.org.js
@@ -0,0 +1,15 @@
+"use strict";
+var coordinates;
+var pieces = location.href.split('/');
+for (let i = 0; i < pieces.length; i++) {
+    if (pieces[i].startsWith('@')) {
+	coordinates = pieces[i].substring(1);
+	break;
+    }
+}
+if (typeof coordinates === 'undefined') {
+    window.alert('Current Google Maps URL does not contain coordinates.');
+} else {
+    const now = new Date();
+    location.href = 'https://www.suncalc.org/#/' + coordinates + '/' + now.getFullYear() + '.' + (now.getMonth() + 1) + '.' + now.getDate() + '/' + now.getHours() ':' + now.getMinutes() + '/1/3';
+}
