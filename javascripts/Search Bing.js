e6a2d9591ca03efa70a687ce72e66746c8ba50c2
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-10-14 17:32:31 +0530

    Add searches

---
+++ javascripts/Search Bing.js
@@ -0,0 +1,12 @@
+"use strict";
+var selection;
+if (window.getSelection) {
+    selection = window.getSelection().toString();
+} else if (document.selection && document.selection.type !== "Control") {
+    selection = document.selection.createRange().text;
+}
+if (selection === undefined) {
+    window.alert('Select the desired text to search.');
+} else {
+    window.open('https://www.bing.com/search?q=' + encodeURIComponent(selection.trim());, '_blank').focus();
+}
