d07c9b48af5dd927d205b9e188387708a43673f3
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-10-14 17:44:35 +0530

    Update Search site.js

--- javascripts/Search site.js
+++ javascripts/Search site.js
@@ -1,2 +1,2 @@
 "use strict";
-location.href = 'https://www.google.com/search?q=site%3A' + encodeURIComponent(location.hostname);
+location.href = 'https://www.google.com/search?udm=14&q=site%3A' + encodeURIComponent(location.hostname);
