42e8e43bb795eb91d71ecce6cd38004ca55d6b97
Thomas Leplus <thomasleplus@users.noreply.github.com>
2024-10-26 10:45:32 +0530

    Add Google Maps search

---
+++ javascripts/Search Google Maps.js
@@ -0,0 +1,12 @@
+"use strict";
+var selection;
+if (window.getSelection) {
+    selection = window.getSelection().toString();
+} else if (document.selection && document.selection.type !== "Control") {
+    selection = document.selection.createRange().text;
+}
+if (selection === undefined) {
+    window.alert('Select the desired text to search.');
+} else {
+    window.open('https://www.google.com/maps/search/' + encodeURIComponent(selection.trim()), '_blank').focus();
+}
