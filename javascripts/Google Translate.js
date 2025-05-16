20cee859117662e234ac8c5a0c3716624a458537
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-05-16 19:01:50 +0200

    Update Google Translate.js

--- javascripts/Google Translate.js
+++ javascripts/Google Translate.js
@@ -2,7 +2,7 @@
 var selection;
 if (window.getSelection) {
     selection = window.getSelection().toString();
-} else if (document.selection && document.selection.type != "Control") {
+} else if (document.selection && document.selection.type !== "Control") {
     selection = document.selection.createRange().text;
 }
 if (selection === undefined) {
