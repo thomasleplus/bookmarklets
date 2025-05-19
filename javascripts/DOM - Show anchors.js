fd7a209a547c7565c2c2927e03707f03e322a9cb
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-05-19 21:56:48 +0200

    Update DOM - Show anchors.js

--- javascripts/DOM - Show anchors.js
+++ javascripts/DOM - Show anchors.js
@@ -1,8 +1,9 @@
 "use strict";
-var i, id;
+var i, anchor, id;
 var anchors = document.getElementsByTagName("a");
 for (i = 0; i < anchors.length; i += 1) {
-    id = anchors[i].id;
+    anchor = anchors[i];
+    id = anchor.id;
     if (id !== undefined && id.length > 0) {
         anchor.appendChild(document.createTextNode(" #" + id));
     }
