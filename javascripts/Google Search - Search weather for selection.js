cbb4f5ae69786d5d62a2980a3d8b2f997dc09912
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-05-19 19:20:45 +0200

    Add two new Google searches

---
+++ javascripts/Google Search - Search weather for selection.js
@@ -0,0 +1,12 @@
+"use strict";
+var selection;
+if (window.getSelection) {
+    selection = window.getSelection().toString();
+} else if (document.selection && document.selection.type !== "Control") {
+    selection = document.selection.createRange().text;
+}
+if (selection === undefined || selection.length === 0) {
+    window.alert('Select the desired text to search.');
+} else {
+    window.open('https://www.google.com/search?q=weather%3A' + encodeURIComponent(selection.trim()), '_blank').focus();
+}
