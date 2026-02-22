f7a1dd84e313b6f31d102f8e38c753c963dcd32d
Thomas Leplus <thomasleplus@users.noreply.github.com>
2026-02-22 19:59:33 +0100

    feat(copypaste): linting

--- javascripts/DOM - Allow copy paste.js
+++ javascripts/DOM - Allow copy paste.js
@@ -1,22 +1,30 @@
-const blockedEvents = ['copy', 'paste', 'cut', 'selectstart', 'contextmenu', 'dragstart', 'select'];
+const blockedEvents = [
+  "copy",
+  "paste",
+  "cut",
+  "selectstart",
+  "contextmenu",
+  "dragstart",
+  "select",
+];
 const _addEventListener = EventTarget.prototype.addEventListener;
-EventTarget.prototype.addEventListener = function(type, fn, options) {
+EventTarget.prototype.addEventListener = function (type, fn, options) {
   if (blockedEvents.includes(type)) return;
   _addEventListener.call(this, type, fn, options);
 };
-document.querySelectorAll('*').forEach(el => {
+document.querySelectorAll("*").forEach((el) => {
   const clone = el.cloneNode(true);
   el.parentNode && el.parentNode.replaceChild(clone, el);
-  blockedEvents.forEach(evt => {
-    clone.removeAttribute('on' + evt);
+  blockedEvents.forEach((evt) => {
+    clone.removeAttribute("on" + evt);
     clone[`on${evt}`] = null;
   });
 });
-blockedEvents.forEach(evt => {
-  document.addEventListener(evt, e => e.stopImmediatePropagation(), true);
+blockedEvents.forEach((evt) => {
+  document.addEventListener(evt, (e) => e.stopImmediatePropagation(), true);
   document[`on${evt}`] = null;
 });
-const style = document.createElement('style');
+const style = document.createElement("style");
 style.textContent = `
   * {
     -webkit-user-select: text !important;
@@ -28,9 +36,9 @@ style.textContent = `
 `;
 document.head.appendChild(style);
 const _dispatchEvent = EventTarget.prototype.dispatchEvent;
-EventTarget.prototype.dispatchEvent = function(e) {
+EventTarget.prototype.dispatchEvent = function (e) {
   if (blockedEvents.includes(e.type) && e.cancelable) {
-    Object.defineProperty(e, 'defaultPrevented', { get: () => false });
+    Object.defineProperty(e, "defaultPrevented", { get: () => false });
   }
   return _dispatchEvent.call(this, e);
 };
